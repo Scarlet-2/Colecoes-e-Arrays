e619ce9a8b718c30288b6d9ecdad5721a68dd425
Matheus Silverio Silva <drchaos820@gmail.com>
2023-03-06 18:46:56 -0300

    Criação de Array e utilização de Sort para achar o centro do Array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -64,6 +64,29 @@ void TentarBuscarPalavra()
     }
 }
 
-TentarBuscarPalavra();
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
 
+//[5,9][1,8][7,1][10][6,9]
+TestaMediana(amostra);
 
+void TestaMediana(Array array)
+{
+    if((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
+    }
+    
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+    //[1,8][5,9][6,9][7,1][10]
+    
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :(numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+    Console.WriteLine($"Com base na amostra a mediana = {mediana}");
+}
