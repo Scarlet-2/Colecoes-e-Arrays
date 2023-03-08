5843b59be11eac3215bcb7b89c019a70a9a9f608
Matheus Silverio Silva <drchaos820@gmail.com>
2023-03-08 18:23:59 -0300

    Mudanças no sistema de Conta Corrente e criação de menção no Program usando for loop

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,92 +1,111 @@
-﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+﻿using bytebank.Modelos.Conta;
 
-void TestArray()
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+//TestaArrayInt();
+//TestaBuscarPalavra();
+
+void TestaArrayInt()
 {
-    int[] numeros = new int[5];
-    numeros[0] = 23;
-    numeros[1] = 15;
-    numeros[2] = 10;
-    numeros[3] = 2;
-    numeros[4] = 300;
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
 
-    //Console.WriteLine($"Tamanho de Array = {numeros.Length}");
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
 
-    int contador = 0;
-    int final = 0;
-    for (int i = 0; i < numeros.Length; i++)
+    int acumulador = 0;
+    for (int i = 0; i < idades.Length; i++)
     {
-        contador = contador + numeros[i];
-        // contador += numeros[i];
+        int idade = idades[i];
+        Console.WriteLine($"índice [{i}] = {idade}");
+        acumulador += idade;
     }
-    
-    // Inciar e declarar arrays
-    // string [] palavras= new string[5] {"André","Jose","Andressa","Neia","Sarah"}
 
-    contador /= numeros.Length;
-    Console.WriteLine(contador);
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
 }
 
-void TentarBuscarPalavra()
+void TestaBuscarPalavra()
 {
-    string[] arraydePalavras = new string[5];
-    for (int i = 0; i < arraydePalavras.Length; i++)
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0; i < arrayDePalavras.Length; i++)
     {
-        Console.Write($"Digite {i + 1} Palavra: ");
-        arraydePalavras[i] = Console.ReadLine();
+        Console.Write($"Digite {i + 1}ª Palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
     }
 
-    Console.Write("Digite a palavra a ser encontrada: ");
-    string busca = Console.ReadLine();
+    Console.Write("Digite palavara a ser encontrada: ");
+    var busca = Console.ReadLine();
 
-    foreach (string palavra in arraydePalavras)
+    foreach (string palavra in arrayDePalavras)
     {
-        // Versão mais complexa
-        /*
-        int indexo = Array.IndexOf(arraydePalavras, palavra);
-        if (palavra.ToLower() == busca.ToLower())
-        {
-            Console.WriteLine($"Voçê digitiou {busca} e foi achada {palavra} e o indexo da palavra é {indexo}.");
-            break;
-        }
-        */
-
         if (palavra.Equals(busca))
         {
-            Console.WriteLine($"Palavra encontrada = {busca}");
+            Console.WriteLine($"Palavra encontrada = {busca}.");
             break;
         }
-        /*
-        else
-        {
-            Console.WriteLine("Palavra não encontrada");
-        }
-        */
     }
+
 }
 
-Array amostra = Array.CreateInstance(typeof(double), 5);
+/*
+Array amostra = new double[5];
 amostra.SetValue(5.9, 0);
 amostra.SetValue(1.8, 1);
 amostra.SetValue(7.1, 2);
 amostra.SetValue(10, 3);
 amostra.SetValue(6.9, 4);
+*/
 
 //[5,9][1,8][7,1][10][6,9]
-TestaMediana(amostra);
+//TestaMediana(amostra);
 
 void TestaMediana(Array array)
 {
-    if((array == null) || (array.Length == 0))
+    if ((array == null) || (array.Length == 0))
     {
         Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
     }
-    
+
     double[] numerosOrdenados = (double[])array.Clone();
     Array.Sort(numerosOrdenados);
     //[1,8][5,9][6,9][7,1][10]
-    
+
     int tamanho = numerosOrdenados.Length;
     int meio = tamanho / 2;
-    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :(numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :
+                                   (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
     Console.WriteLine($"Com base na amostra a mediana = {mediana}");
 }
+
+/*
+int[] valores = { 10, 58, 36, 47 };
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(valores[i]);
+}
+*/
+
+void TestaArrayDeContasCorrentes()
+{
+    ContaCorrente[] listaDeContas = new ContaCorrente[]
+    {
+        // Lista de (Objetos) Contas Corrente
+        new ContaCorrente(874, "4239787-B"),
+        new ContaCorrente(874, "4456456-A"),
+        new ContaCorrente(874, "7764144-C")
+    };
+    
+    // Passa por cada item 
+    for(int i = 0; i < listaDeContas.Length; i++)
+    {
+        ContaCorrente contaAtual = listaDeContas[i];
+        Console.WriteLine($"Índice{i} - Conta:{contaAtual.Conta}");
+    }
+}
+
+TestaArrayDeContasCorrentes();
